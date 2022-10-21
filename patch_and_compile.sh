9a49c34822e376ded9e2af37aa583ea4b2d0bf21
Conrad Chan <conradchan@hotmail.com.au>
2022-10-21 11:41:29 +1100

    add compile to script, and rename script

--- patch_and_compile.sh
+++ patch_and_compile.sh
@@ -4,4 +4,9 @@ set -e
 PYNN=$(python -c 'import pyNN; print(pyNN.__path__[0])')
 echo "Changing directory to $PYNN"
 cd $PYNN
+
+# Apply patches
 diff -ru . $OLDPWD/Cortex_BasalGanglia_DBS_model/Updated_PyNN_Files/ | patch -p0
+
+# Compile
+nrnivmodl
