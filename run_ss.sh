9df0b0ed542b76720252b3a3c32615786e846c9c
David Liptai <dliptai@swin.edu.au>
2022-10-24 16:50:37 +1100

    plot number and fail run script if sim fails

--- run_ss.sh
+++ run_ss.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
+set -e
 
 NP="${1:-1}"
 
 cd Cortex_BasalGanglia_DBS_model
 mpirun -np $NP time nrniv -nogui -python -mpi run_CBG_Model_to_SS.py
 cd $OLDPWD
-./plot_ss.py
+./plot_ss.py $NP
