f5f57e969536cbf380ab0da0446fb31009ff7ece
David Liptai <dliptai@swin.edu.au>
2022-10-24 16:49:27 +1100

    save plots

--- plot_ss.py
+++ plot_ss.py
@@ -2,15 +2,22 @@
 
 from matplotlib import pyplot as plt
 from neo.io import NeoMatlabIO
+import sys
 
-filename = 'Cortex_BasalGanglia_DBS_model/Simulation_Output_Results/Steady_State_Simulation/STN_LFP.mat'
+filename = "Cortex_BasalGanglia_DBS_model/Simulation_Output_Results/Steady_State_Simulation/STN_LFP.mat"
 
 block = NeoMatlabIO(filename).read_block()
 signal = block.segments[0].analogsignals[0]
 
 plt.plot(signal)
 
-plt.xlabel('Time')
-plt.ylabel('Signal')
+plt.xlabel("Time")
+plt.ylabel("Signal")
 
-plt.show()
+# plt.show()
+
+if len(sys.argv) > 1:
+    n = sys.argv[1]
+else:
+    n = 1
+plt.savefig(f"plot_{n}.pdf")
