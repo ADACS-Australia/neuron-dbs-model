0319dab966578a60117952ccb1016b8df7114b5f
David Liptai <dliptai@swin.edu.au>
2022-10-24 11:59:35 +1100

    no gui

--- run_ss.sh
+++ run_ss.sh
@@ -3,6 +3,6 @@
 NP="${1:-1}"
 
 cd Cortex_BasalGanglia_DBS_model
-mpirun -np $NP time nrniv -python -mpi run_CBG_Model_to_SS.py
+mpirun -np $NP time nrniv -nogui -python -mpi run_CBG_Model_to_SS.py
 cd $OLDPWD
 ./plot_ss.py
