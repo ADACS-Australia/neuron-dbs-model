bd1d70071fbead3055a945490969edc5c0e9f95d
David Liptai <dliptai@swin.edu.au>
2022-10-24 20:31:08 +1100

    make plotting script more generic

--- run_ss.sh
+++ run_ss.sh
@@ -6,4 +6,4 @@ NP="${1:-1}"
 cd Cortex_BasalGanglia_DBS_model
 mpirun -np $NP time nrniv -nogui -python -mpi run_CBG_Model_to_SS.py
 cd $OLDPWD
-./plot_ss.py $NP
+./plot_ss.py ./Cortex_BasalGanglia_DBS_model/Simulation_Output_Results/Steady_State_Simulation/STN_LFP.mat
