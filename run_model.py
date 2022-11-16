84148e72653a88172ae80712d0f0659378cf8077
David Liptai <dliptai@swin.edu.au>
2022-11-16 16:24:32 +1100

    rename file

--- run_model.py
+++ run_model.py
@@ -41,7 +41,7 @@ import numpy as np
 import math
 import argparse
 from py.utils import make_beta_cheby1_filter, calculate_avg_beta_power
-from py.model import load_network, electrode_distance
+from py.network import load_network, electrode_distance
 from py.config import Config, get_controller_kwargs
 
 # Import global variables for GPe DBS
