87aad77b4e2452f019157d90b8b44bfda6af5929
David Liptai <dliptai@swin.edu.au>
2022-11-16 17:05:33 +1100

    fix installation order
    Install neuron before everything else
    Then we don't have to run nrnivmodl in the installation directory manually

--- patch_and_compile.sh
+++ patch_and_compile.sh
@@ -1,22 +1,13 @@
 #!/usr/bin/env bash
 set -e
 
-MODEL="Cortex_BasalGanglia_DBS_model"
-
-echo "Changing directory to $MODEL"
-cd $MODEL
-# Compile
+echo "--> Building MOD files"
+cd Cortex_BasalGanglia_DBS_model/
 nrnivmodl
-cd $OLDPWD
 
 PYNN=$(python -c 'import pyNN; print(pyNN.__path__[0])')
-echo "Changing directory to $PYNN"
+echo "--> Changing directory to $PYNN"
 cd $PYNN
 
 # Apply patches
-diff -ru . $OLDPWD/$MODEL/Updated_PyNN_Files/ | patch -p0 || echo "WARNING: Assuming pyNN is already patched."
-
-# Compile
-cd neuron/nmodl
-echo "Changing directory to $(pwd)"
-nrnivmodl
+diff -ru . $OLDPWD/Cortex_BasalGanglia_DBS_model/Updated_PyNN_Files/ | patch -p0 || echo "WARNING: Assuming pyNN is already patched."
