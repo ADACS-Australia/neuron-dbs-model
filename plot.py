23509ed885b9eeabbfabbea96cba678fe2455d0a
David Liptai <dliptai@swin.edu.au>
2022-11-16 15:04:39 +1100

    fix plotting

--- plot.py
+++ plot.py
@@ -4,9 +4,10 @@ from matplotlib import pyplot as plt
 from neo.io import NeoMatlabIO
 import argparse
 
-parser = argparse.ArgumentParser(description="Plot STN_LFP.mat files")
-parser.add_argument('filename', help='file to plot')
-parser.add_argument('-s','--save',action='store_true',default=False)
+parser = argparse.ArgumentParser(description="Plot *.mat files")
+parser.add_argument("filename", help="file to plot")
+parser.add_argument("-s", "--save", action="store_true", default=False, help='save the plot')
+parser.add_argument("-o", "--output", default="plot.pdf", help="output file name (ignored unless -s is specified. Default: plot.pdf)")
 args = parser.parse_args()
 
 block = NeoMatlabIO(args.filename).read_block()
