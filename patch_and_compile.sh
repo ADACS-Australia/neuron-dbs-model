76397f73bfba1370d3f850ed658e8c86f891ddc0
David Liptai <dliptai@swin.edu.au>
2022-11-16 17:16:12 +1100

    fix patching

--- patch_and_compile.sh
+++ patch_and_compile.sh
@@ -7,4 +7,4 @@ nrnivmodl hoc/
 PYNN=$(python -c 'import pyNN; print(pyNN.__path__[0])')
 echo "--> Applying PyNN patches"
 cd $PYNN
-diff -ru . $OLDPWD/Cortex_BasalGanglia_DBS_model/Updated_PyNN_Files/ | patch -p0 || echo "WARNING: Assuming pyNN is already patched."
+diff -ru . $OLDPWD/Updated_PyNN_Files/ | patch -p0 || echo "WARNING: Assuming pyNN is already patched."
