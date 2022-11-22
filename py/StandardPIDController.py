dd4c1910b491b6db37bf307070ca267a3c2a82a2
David Liptai <dliptai@swin.edu.au>
2022-11-22 16:04:06 +1100

    PID inherits from Zero controller

--- py/StandardPIDController.py
+++ py/StandardPIDController.py
@@ -22,7 +22,6 @@ class StandardPIDController:
     """Standard PID Controller Class"""
 
     label = "Standard_PID_Controller"
-    units = "mA"
 
     def __init__(
         self,
@@ -34,23 +33,7 @@ class StandardPIDController:
         minvalue=0.0,
         maxvalue=1e9,
     ):
-        # Initial Controller Values
-        self.setpoint = setpoint
-        self.ts = ts  # should be in sec
-
-        self.current_time = 0.0  # (sec)
-        self.last_time = 0.0
-        self.last_error = 0.0
-        self.last_output_value = 0.0
-
-        # Initialize the output value of the controller
-        self.output_value = 0.0
-
-        # Lists for tracking controller history
-        self.state_history = []
-        self.error_history = []
-        self.output_history = []
-        self.sample_times = []
+        super().__init__(setpoint,ts)
 
         self.kp = kp
         self.ti = ti
@@ -67,27 +50,11 @@ class StandardPIDController:
     def clear(self):
         """Clears controller variables"""
 
-        self.last_error = 0.0
-
-        self.state_history = []
-        self.error_history = []
-        self.output_history = []
-        self.sample_times = []
-
-        self.last_output_value = 0.0
-        self.output_value = 0.0
+        super().clear()
 
         self.ITerm = 0.0
         self.DTerm = 0.0
 
-    def get_error(self, state_value):
-        # Calculate Error - if setpoint > 0.0, then normalize error with
-        # respect to set point
-        if self.setpoint == 0.0:
-            error = state_value - self.setpoint
-        else:
-            error = (state_value - self.setpoint) / self.setpoint
-        return error
 
     def set_output(self, state_value):
         """Calculates controller output signal for given reference feedback
@@ -133,26 +100,3 @@ class StandardPIDController:
             self.output_value = u
 
         return error
-
-    def update(self, state_value, current_time):
-        """Update controller state"""
-        self.current_time = current_time
-
-        error = self.set_output(state_value)
-
-        # Remember last time and last error for next calculation
-        self.last_time = self.current_time
-        self.last_error = error
-
-        # Update the last output value
-        self.last_output_value = self.output_value
-
-        # Record state, error, y(t), and sample time values
-        self.state_history.append(state_value)
-        self.error_history.append(error)
-        self.output_history.append(self.output_value)
-        # Convert from msec to sec
-        self.sample_times.append(current_time / 1000)
-
-        # Return controller output
-        return self.output_value
