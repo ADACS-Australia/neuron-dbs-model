1bfb1d5ff419aeeac927813213ff91700ed8adc7
David Liptai <dliptai@swin.edu.au>
2022-11-25 12:47:18 +1100

    use patch instead of Update_PyNN_Files

--- patch_and_compile.sh
+++ patch_and_compile.sh
@@ -6,5 +6,6 @@ nrnivmodl hoc/
 
 PYNN=$(python -c 'import pyNN; print(pyNN.__path__[0])')
 echo "--> Applying PyNN patches"
+echo "Found PyNN install dir: $PYNN/"
 cd $PYNN
-diff -ru . $OLDPWD/Updated_PyNN_Files/ | patch -p0 || echo "WARNING: Assuming pyNN is already patched."
+patch -p2 -Ni $OLDPWD/pynn.patch -r $OLDPWD/.rej || { echo "WARNING: Assuming pyNN is already patched. Removing .rej file"; rm $OLDPWD/.rej; }
